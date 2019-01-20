2c3c4600a0301531a94904de7a1e8a37e67c4bff
yqbjtu <yqbjtu@163.com>
2019-01-20 13:16:07 +0800

    clean the read specified offset topic demo

--- KafkaCLI/src/main/java/com/yq/consumer/ReceiveLatestMessageMain.java
+++ KafkaCLI/src/main/java/com/yq/consumer/ReceiveLatestMessageMain.java
@@ -16,15 +16,14 @@ import org.apache.kafka.common.TopicPartitionInfo;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
 /**
- * 本例子试着读取当前topci最新的30条消息(如果topic内有没有30条，就获取实际消息)
- * className: SendMessageMain
+ * 本例子试着读取当前topic每个分区内最新的30条消息(如果topic额分区内有没有30条，就获取实际消息)
+ * className: ReceiveLatestMessageMain
  *
  * @author EricYang
  * @version 2019/01/10 11:30
@@ -39,7 +38,7 @@ public class ReceiveLatestMessageMain {
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "yq-consumer11");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "yq-consumer12");
         props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 20);
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
         props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
@@ -48,8 +47,6 @@ public class ReceiveLatestMessageMain {
 
 
         System.out.println("create KafkaConsumer");
-
-        System.out.println("receive data");
         final KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
         AdminClient adminClient = AdminClient.create(props);
         String topic = "topic01";
@@ -87,70 +84,7 @@ public class ReceiveLatestMessageMain {
 
                 });
             }
-            //topicResult.
 
-            consumer.subscribe(Arrays.asList(topic));
-            boolean setOffset = false;
-            Map<String, Long> map = new HashMap<>();
-
-            /*while(!setOffset) {
-                long pollStartTime = System.currentTimeMillis();
-                System.out.println("start to pool. " + pollStartTime);
-                ConsumerRecords<String, String> firstRecords = consumer.poll(1000);
-
-                long pollEndTime =  System.currentTimeMillis();
-                System.out.println("end to pool. diff:" + (pollEndTime - pollStartTime));
-                for (ConsumerRecord<String, String> record : firstRecords) {
-                    System.out.printf("set offset =%d, key=%s , value= %s, partition=%s\n",
-                            record.offset(), record.key(), record.value(), record.partition());
-                }
-                for (TopicPartition partition : firstRecords.partitions()) {
-                    long position = consumer.position(partition);
-                    Map<TopicPartition, Long> mapBeginning = consumer.beginningOffsets(Arrays.asList(partition));
-                    Iterator<Map.Entry<TopicPartition, Long>> itr2 = mapBeginning.entrySet().iterator();
-                    while(itr2.hasNext()) {
-                        Map.Entry<TopicPartition, Long> entry = itr2.next();
-                        System.out.println("beginningOffset of partitionId: " + entry.getKey().partition() + "  is " + entry.getValue());
-                    }
-                    Map<TopicPartition, Long> mapEnd = consumer.endOffsets(Arrays.asList(partition));
-                    Iterator<Map.Entry<TopicPartition, Long>> itr3 = mapEnd.entrySet().iterator();
-                    while(itr3.hasNext()) {
-                        Map.Entry<TopicPartition, Long> entry = itr3.next();
-                        System.out.println("endOffset of partitionId: " + entry.getKey().partition() + "  is " + entry.getValue());
-                    }
-
-                    log.info("partitionId={}, position={}", partition.partition(), position);
-                    List<ConsumerRecord<String, String>> partitionRecords = firstRecords.records(partition);
-
-                    long lastOffset = partitionRecords.get(partitionRecords.size() - 1).offset();
-                    long expectedOffSet = lastOffset - 1 - COUNT;
-                    expectedOffSet = expectedOffSet > 0? expectedOffSet : 1;
-                    int partitionId = partition.partition();
-                    System.out.printf("currentOffset=%s for positionId=%s \n", expectedOffSet,  partitionId);
-                    Long existingOffset = map.get(partitionId);
-                    if (existingOffset == null) {
-                        map.put(String.valueOf(partitionId), expectedOffSet);
-                        System.out.printf("ActualCommitOffset=%s for positionId=%s \n", expectedOffSet,  partitionId);
-                        consumer.commitSync(Collections.singletonMap(partition, new OffsetAndMetadata(expectedOffSet -1 )));
-                    }
-                    else {
-                        if (expectedOffSet < existingOffset) {
-                            map.put(String.valueOf(partitionId), expectedOffSet);
-                            System.out.printf("ActualCommitOffset=%s for positionId=%s \n", expectedOffSet,  partitionId);
-                            consumer.commitSync(Collections.singletonMap(partition, new OffsetAndMetadata(expectedOffSet -1 )));
-                        }
-                        else {
-                            System.out.printf("existingOffset=%s, expectedOffSet=%s for positionId=%s \n", existingOffset, expectedOffSet, partitionId);
-                        }
-                    }
-
-                    setOffset = true;
-                }
-            }*/
-
-            //consumer.close();
-            //props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
-            //consumer = new KafkaConsumer<>(props);
             consumer.subscribe(Arrays.asList(topic));
             while (true) {
                 ConsumerRecords<String, String> records = consumer.poll(100);
@@ -167,7 +101,6 @@ public class ReceiveLatestMessageMain {
             consumer.close();
         }
     }
-
 }
 
 /*
