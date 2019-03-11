10fb1dd135df1248b143f868ec85638dfccb8dbf
yqbjtu <yqbjtu@163.com>
2019-03-11 11:51:27 +0800

    add the appropriate slf4j implementation log4j

--- KafkaStreamDemo/src/main/java/com/yq/LogMainTest.java
+++ KafkaStreamDemo/src/main/java/com/yq/LogMainTest.java
@@ -12,7 +12,7 @@ import org.slf4j.LoggerFactory;
  * @version 2019/3/7 9:14
  */
 public class LogMainTest {
-    private static final org.slf4j.Logger log = LoggerFactory.getLogger(LogMainTest.class);
+    private static final Logger log = LoggerFactory.getLogger(LogMainTest.class);
 
     public static void main(final String[] args) throws Exception {
         log.info("info");
